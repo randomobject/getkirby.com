0badbe0f00da410e17587bd587c6a7acc8ff0aca
Bastian Allgeier <mail@bastianallgeier.com>
2018-05-24 11:38:24 +0200

    Improve newsletter form

--- site/snippets/kosmos.subscription.php
+++ site/snippets/kosmos.subscription.php
@@ -5,7 +5,7 @@
 
   <form action="https://getkirby.createsend.com/t/t/s/xdildi/" method="post" id="subForm">
     <p>
-      <label for="fieldEmail">Email</label>
+      <label for="fieldEmail">Email <small>(required)</small></label>
       <input id="fieldEmail" name="cm-xdildi-xdildi" type="email" placeholder="mail@example.com" required />
     </p>
     <p>
@@ -16,5 +16,7 @@
       <button type="submit">Subscribe</button>
     </p>
   </form>
+
+  <p class="newsletter-meta">By subscribing you agree that we send you monthly newsletters. We won't ever spam you! You can unsubscribe any time. We use the Australian company CampaignMonitor to collect subscribers and send out newsletters. Find out more in our <a href="<?= url('privacy#newsletter') ?>">pricacy policy</a>.</p>
 
 </section>
