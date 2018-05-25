b458eaf0250316f820ea2dd75c78830f05a34f9e
Bastian Allgeier <mail@bastianallgeier.com>
2018-05-25 15:54:32 +0200

    Remove imgix partner logo

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,11 +4,8 @@
     <div class="sponsors">
       <p class="sponsors-headline">Our partners</p>
       <p class="sponsors-logos">
-        <a class="sponsors-keycdn" href="https://www.keycdn.com/?a=5715">        
+        <a class="sponsors-keycdn" href="https://www.keycdn.com/?a=5715">
           <img src="<?php echo url('assets/images/keycdn.svg') ?>" alt="CDN by KeyCDN">
-        </a>
-        <a class="sponsors-imgix" href="https://www.imgix.com/?utm_source=kirby">    
-          <img src="<?php echo url('assets/images/imgix.svg') ?>" alt="Image hosting by imgIX">
         </a>
         <a class="sponsors-algolia" href="https://algolia.com">
           <img src="<?php echo url('assets/images/algolia.svg') ?>" alt="Search by Algolia">
